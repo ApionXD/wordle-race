8e0b9cdee882547498a6af69bf1c9096b534272c
Stella <stella@pop-os.localdomain>
2022-02-20 16:20:04 -0500

    added duplicated handling and main function

--- api_draft.py
+++ api_draft.py
@@ -1,37 +1,59 @@
 import collections
 from wonderwords import RandomWord
 
+# setup
 r = RandomWord()
-wotd = 'toast'
-# print 5 test words:
-for i in range(5):
-    print(r.word(include_parts_of_speech = ['nouns', 'verbs', 'adjectives'], word_min_length = 5, word_max_length = 5))
+wotd = ''
+
+def generateWotd(pos = ['nouns', 'verbs', 'adjectives'], length = 5):
+    # defaults to nouns/verbs/adjectives of length 5
+    return r.word(include_parts_of_speech = pos, word_min_length = length, word_max_length = length)
 
 def validate(s):
-    if s.isalpha():
-        return 'valid'
-    else:
-        return 'invalid'
+    return s.isalpha()
 
 def verifyGuess(guess):
-    returnArray = ['','','','','']
-    # check for duplicates in the wotd and the guess
+    if not validate(guess):
+        print('ERROR: INVALID GUESS')
+        return 0
+    guess = guess.lower()
+    returnArray = [tuple(('',0)) for x in range(len(guess))]
+    
+    # check for duplicates in the wotd
     wotdDupes = collections.defaultdict(int)
-    guessDupes = collections.defaultdict(int)
     for c in wotd:
         wotdDupes[c] += 1
-    for c in guess:
-        wotdDupes[c] += 1
+
     # check each letter
     # 2 = correct, 1 = correct letter wrong location, 0 = wrong
-    for i in range(5):
+    # also include index for resorting
+    # first pass
+    for i, c in enumerate(guess):
         if guess[i] == wotd[i]:
             returnArray[i] = tuple((guess[i], 2))
+    # second pass
+    for i, c in enumerate(guess):
+        if guess[i] == wotd[i]:
+            pass
         elif guess[i] in wotd:
-            # REMINDER: handle duplicates later
-            returnArray[i] = tuple((guess[i], 1))
+            # handle duplicates
+            if tuple((guess[i], 2)) in returnArray or tuple((guess[i], 1)) in returnArray:
+                if sum(1 for t in returnArray if t[0] == guess[i]) >= wotdDupes[guess[i]]:
+                    returnArray[i] = tuple((guess[i], 0))
+                else:
+                    returnArray[i] = tuple((guess[i], 1))
+            else:
+                returnArray[i] = tuple((guess[i], 1))
         else:
             returnArray[i] = tuple((guess[i], 0))
     return returnArray
 
-print(verifyGuess('shoot'))
+def main():
+    global wotd
+    wotd = 'toast'
+    # wotd = generateWotd()
+    print(wotd)
+    print(verifyGuess('shoot'))
+
+if __name__ == "__main__":
+    main()
