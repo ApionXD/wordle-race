387d7c824a5f4998e4b08f828c7691c360cba92b
ApionXD <apion@apionservers.com>
2022-02-20 17:23:52 -0500

    Add some checks into what can be typed into the wordle board

--- wordle-race-web/src/components/WordleBoard.tsx
+++ wordle-race-web/src/components/WordleBoard.tsx
@@ -8,6 +8,7 @@ type BoardProps = {
 export default function WordleBoard(props: BoardProps) {
     const [rowWords, setRowWords]: [string[], any] = useState([])
     const [curRow, setCurRow]: [number, any] = useState(0)
+    const [responseText, setResponseText] = useState("")
     let rows: ReactElement[] = []
     for (let i = 0; i < props.length; i++) {
         rows.push(<WordRow length={props.length} letters={rowWords[i]}/>)
@@ -23,13 +24,21 @@ export default function WordleBoard(props: BoardProps) {
             newRowWords[curRow] = newRowWords[curRow] ? newRowWords[curRow].slice(0, -1) : newRowWords[curRow]
         }
         if (event.key == 'Enter') {
-            setCurRow(curRow + 1)
+            if (newRowWords[curRow].length == 5) {
+                setCurRow(curRow + 1)
+            }
+            else {
+                setResponseText("Please enter a word of correct length")
+            }
         }
-        else if (event.key.length == 1) {
-            newRowWords[curRow] = newRowWords[curRow] ? newRowWords[curRow] + event.key : event.key
+        else if (event.key.length == 1 && event.key.match("[a-zA-Z]+")?.length == 1) {
+            newRowWords[curRow] = newRowWords[curRow] ? newRowWords[curRow] + event.key.toUpperCase() : event.key.toUpperCase()
         }
 
         setRowWords(newRowWords)
         console.log(rowWords[curRow])
-    }} tabIndex={0}>{rows}</div>
+    }} tabIndex={0}>
+        <p>{responseText}</p>
+        {rows}
+    </div>
 }
