6c71e7fa245213a63baaabddf48066e004db9274
ApionXD <apion@apionservers.com>
2022-03-15 16:41:46 -0400

    Added index page

--- wordle-race-web/src/App.tsx
+++ wordle-race-web/src/App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
-import {Routes} from "react-router-dom";
+import {Link, Routes, Outlet} from "react-router-dom";
 import {Route} from "react-router";
 import WordRow from "./components/WordRow";
 import WordleBoard from "./components/WordleBoard";
+import LoginPage from "./components/LoginPage"
 
 function App() {
   return (
     <div className="App">
       <Routes>
           <Route path={"/"} element={
+              <nav>
+                  <Link to={"board"}>Board</Link><br/>
+                  <Link to={"login"}>Login</Link>
+              </nav>
+          }/>
+          <Route path={"/board"} element={
+                  <div>
+                      <WordleBoard length={5} height={5} />
+                  </div>
+              }> </Route>
+          <Route path={"/login"} element={
               <div>
-                  <WordleBoard length={5} height={5} />
+                  <LoginPage/>
               </div>
-
-          }> </Route>
+          }/>
       </Routes>
     </div>
   );
