9825f892ca2c974ea2b769d2f2e595d27c9635b5
kunle7894 <kunle54321@hotmail.com>
2022-04-20 03:56:46 -0400

    Checked dict for words before accepting. Made score system work/showup on screen. Made types after 5 character not allowed.

--- wordle-race-api/game.py
+++ wordle-race-api/game.py
@@ -1,6 +1,6 @@
 import uuid
 import time
-
+from database import db
 from flask import Blueprint, request, session
 import api_draft
 import json
@@ -8,28 +8,67 @@ import json
 game_blueprint = Blueprint('game_blueprint', __name__)
 current_games = list()
 game_statuses = dict()
-
+game_score = dict()
+user_collection = db["users"]
 
 @game_blueprint.route("/check", methods=['POST'])
 def check_endpoint():
     check_request = request.json
     game_status = game_statuses[check_request['id']]
+    game = getGameByUser(session['username'])
+    player = game.player1 if game.player1 == session['username'] else game.player2
     if game_status == "Completed":
         return json.dumps({
             "response": "Time's up",
         })
-    game = getGameByUser(session['username'])
+
     if time.time() - game.start_time >= game.duration:
         del_game(game)
+        #TODO send points to user
+        user = user_collection.find_one({"username": player})
+        if user is not None:
+            None
+            #change scores need to add table to db
+            #user.update_one(scores, )
         return json.dumps({
             "response": "Time's up",
         })
 
+    if not player in game_score.keys():
+        game_score[player] = [0, [-1]]
+
     guess = request.json['guess']
     board = game.boards[game.player1_board] if game.player1 == session['username'] else game.boards[game.player2_board]
+    checkedGuess = board.verifyGuess(guess)
+    if (checkedGuess==0):
+        return json.dumps({
+            "response": "Failure",
+            "colors": [0]
+        })
+    numGood = 0
+    row = check_request['curRow']
+    pointsAdded = 1
+    if row == 0: #points based on rows
+        pointsAdded *= 5
+    elif row == 1:
+        pointsAdded *= 4
+    elif row == 2:
+        pointsAdded *= 3
+    elif row == 3:
+        pointsAdded *= 2
+    for x in range(len(checkedGuess)): #currently only gives points for greens
+        if checkedGuess[x][1]==2 and not x in game_score[player][1]:
+            game_score[player][1].append(x)
+            game_score[player] = [pointsAdded+game_score[player][0], game_score[player][1]]
+    if len(game_score[player][1])-1==len(guess) or row==4: #resets what is already known green for new game
+        game_score[player][1] = [-1]
+        if row==0: #gives extra points if solved it right on first try
+            game_score[player][0]*=2
+
     return json.dumps({
         "response": "Success",
-        "colors": [x[1] for x in board.verifyGuess(guess)]
+        "colors": [x[1] for x in checkedGuess],
+        "score": game_score[player][0]
     })
 
 def add_game(game):
--- wordle-race-web/src/components/WordleBoard.tsx
+++ wordle-race-web/src/components/WordleBoard.tsx
@@ -15,6 +15,7 @@ export default function WordleBoard(props: BoardProps) {
     const [rowColors, setRowColors]: [number[][], any] = useState([])
     const [curRow, setCurRow]: [number, any] = useState(0)
     const [responseText, setResponseText] = useState("")
+    const [score, setScore] = useState(0)
     let rows: ReactElement[] = []
     for (let i = 0; i < props.length; i++) {
         rows.push(<WordRow length={props.length} letters={rowWords[i]} colors={rowColors[i]}/>)
@@ -34,30 +35,55 @@ export default function WordleBoard(props: BoardProps) {
             if (newRowWords[curRow].length == 5) {
                 axios.post('/check', {
                     "id": state?.gameDetails?.id,
-                    "guess": newRowWords[curRow]
+                    "guess": newRowWords[curRow],
+                    "curRow": curRow
                 }).then(r => {
-                    console.log(r.data)
-                    let newRow = r.data.colors
-                    console.log(newRow)
-                    // check if game is won
-                    if (newRow.every((i: number) => i == 2)) {
-                        setResponseText("You win!")
-                        render(
-                            <button onClick={()=> NewGame()}>New Game</button>
-                        )
-                        // TODO: lock the keyboard, offer play again button
+                    let response = r.data.response
+                    console.log(response)
+                    if (response=="Success")
+                    {
+                        console.log(r.data)
+                        let newRow = r.data.colors
+                        console.log(newRow)
+                        setScore(r.data.score)
+                        // check if game is won or max size
+                        let wonGame = newRow.every((i: number) => i == 2)
+                        if (wonGame || curRow>=4) {
+                            if (wonGame)
+                            {
+                                setResponseText("You win!")
+                            }
+                            render(
+                                <button onClick={()=> NewGame()}>New Game</button>
+                            )
+
+                            // TODO: lock the keyboard, offer play again button
+                        }
+                        newRowColors[curRow] = newRow
+                        setRowColors(newRowColors)
+                        setCurRow(curRow + 1)
+                        setResponseText("")
                     }
-                    newRowColors[curRow] = newRow
-                    setRowColors(newRowColors)
+                    else
+                    {
+                        let temp = newRowWords[curRow]
+                        newRowWords[curRow] = ""
+                        setResponseText(temp.at(0)+temp.substring(1).toLowerCase()+" is not a word")
+                        
+                    }
                 })
-                setCurRow(curRow + 1)
+                
             }
             else {
                 setResponseText("Please enter a word of correct length")
             }
         }
-        else if (event.key.length == 1 && event.key.match("[a-zA-Z]+")?.length == 1) {
+        else if (event.key.length == 1 && event.key.match("[a-zA-Z]+")?.length == 1 && (!newRowWords[curRow] || newRowWords[curRow].length<5)) {
             newRowWords[curRow] = newRowWords[curRow] ? newRowWords[curRow] + event.key.toUpperCase() : event.key.toUpperCase()
+            if (newRowWords[curRow].length>5)
+            {
+                newRowWords[curRow].slice(0, -1)
+            }
         }
 
         setRowWords(newRowWords)
@@ -65,6 +91,7 @@ export default function WordleBoard(props: BoardProps) {
     }} tabIndex={0}>
         <p>{responseText}</p>
         {rows}
+        <p>Score: {score}</p>
     </div>
 }
 
